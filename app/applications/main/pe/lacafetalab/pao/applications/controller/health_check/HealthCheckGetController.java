ff0df4006c56dbdaf3d4bbf5a6676bd661f5d33e
Jose Guillermo <jguillermo@outlook.com>
2020-06-30 00:28:06 -0500

    rest json

--- app/applications/main/pe/lacafetalab/pao/applications/controller/health_check/HealthCheckGetController.java
+++ app/applications/main/pe/lacafetalab/pao/applications/controller/health_check/HealthCheckGetController.java
@@ -1,13 +1,15 @@
 package pe.lacafetalab.pao.applications.controller.health_check;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.lacafetalab.pao.shared.infrastructure.response.RestResponse;
 
 @RestController
 public class HealthCheckGetController {
 
     @GetMapping("/health-check")
-    public String index() {
-        return "ok";
+    public ResponseEntity<RestResponse> index() {
+        return RestResponse.ok();
     }
 }
